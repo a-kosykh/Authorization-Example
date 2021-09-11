52b2d96ab4de1ff4cc591d5668e0044865aef77b
Aleksandr Kosykh <22540979+a-kosykh@users.noreply.github.com>
2021-09-11 12:58:57 +0300

    saving salt to db

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import hashlib
 import getpass
-
+import bcrypt
 
 class DatabaseLayer:
     def __init__(self, name):
@@ -11,16 +11,17 @@ class DatabaseLayer:
     def create_connection(self):
         try:
             self.conn = sqlite3.connect(self.name)
-        except sqlite3.Error as e:
+        except sqlite3.OperationalError as e:
             print(e)
 
     def create_table(self):
         try:
             c = self.conn.cursor()
-            c.execute("CREATE TABLE users"
+            c.execute("CREATE TABLE IF NOT EXISTS users"
                       "(username text NOT NULL,"
-                      "h_password text);")
-            c.execute("CREATE UNIQUE INDEX idx_users_username ON users (username);")
+                      "h_password text,"
+                      "salt text);")
+            c.execute("CREATE UNIQUE INDEX IF NOT EXISTS idx_users_username ON users (username);")
         except sqlite3.Error as e:
             print(e)
 
@@ -39,12 +40,25 @@ class DatabaseLayer:
     def register_user(self, data):
         try:
             c = self.conn.cursor()
-            sql_query = ''' INSERT INTO users(username, h_password) VALUES (?,?) '''
+            sql_query = ''' INSERT INTO users(username, h_password, salt) VALUES (?,?,?) '''
             c.execute(sql_query, data)
             self.conn.commit()
             return True
         except sqlite3.Error as e:
             return False
+
+    def get_salt(self, name):
+        try:
+            c = self.conn.cursor()
+            sql_query = ''' SELECT salt FROM users WHERE username = ? '''
+            c.execute(sql_query, name)
+            self.conn.commit()
+            
+            return c.fetchone()[0]
+        except sqlite3.Error as e:
+            print(e)
+            return False
+
 
     def find_user(self, data):
         c = self.conn.cursor()
@@ -72,8 +86,10 @@ class Auth:
 
     def register(self, name, password):
         if self.db is not None:
-            h_password = hashlib.pbkdf2_hmac('sha256', bytes(password, 'utf-8'), b'salt', 100000).hex()
-            if self.db.register_user((name, h_password)):
+            salt = bcrypt.gensalt()
+            print("salt: ", salt)
+            h_password = hashlib.pbkdf2_hmac('sha256', bytes(password, 'utf-8'), salt, 100000).hex()
+            if self.db.register_user((name, h_password, salt)):
                 print(f"Successful registration of '{name}'!")
             else:
                 print(f"ERROR: User with name '{name}' already exists!")
@@ -86,11 +102,15 @@ class Auth:
             return False
 
     def login(self, name, password):
+        self.db.show_table()
+
         if self.login_limit():
             return False
 
-        h_password = hashlib.pbkdf2_hmac('sha256', bytes(password, 'utf-8'), b'salt', 100000).hex()
-        if db.find_user((name, h_password)):
+        salt = self.db.get_salt((name,))
+
+        h_password = hashlib.pbkdf2_hmac('sha256', bytes(password, 'utf-8'), salt, 100000).hex()
+        if self.db.find_user((name, h_password)):
             print(f"Login complete!")
             self.logged_user = User(name)
             return True
@@ -100,18 +120,26 @@ class Auth:
             return False
 
 
-def init_loop(user):
+
+def init_loop(user, db_obj):
     print(f"\nYour are logged as {user.name}...")
     while True:
         command = input("Enter command: ")
         if command == 'exit':
             break
+        if command == 'show_table':
+            db_obj.show_table()
 
+def main():
+    db_name = "mydb.db"
 
-if __name__ == '__main__':
-    db = DatabaseLayer("mydb.db")
+    db = DatabaseLayer(db_name)
     db.create_connection()
-    # db.create_table()
+    
+    if db.conn != None: 
+        db.create_table()
+    else:
+        return -1;
 
     auth = Auth(db, 3)
 
@@ -129,10 +157,13 @@ if __name__ == '__main__':
             name = input("Enter username: ")
             password = input("Enter password: ")
             if auth.login(name, password):
-                init_loop(auth.logged_user)
+                init_loop(auth.logged_user, db)
                 break
             continue
 
         if code == '3':
             print("\nExiting....")
             break
+
+if __name__ == '__main__':
+    main()
